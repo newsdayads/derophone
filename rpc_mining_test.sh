bbd7f95757dea257313e9c8f4cef7cfec6956699
newsdayads <125233768+newsdayads@users.noreply.github.com>
2023-04-17 06:48:26 +0700

    Add files via upload

--- rpc_mining_test.sh
+++ rpc_mining_test.sh
@@ -4,6 +4,6 @@ while :; do
     rand_num=$((RANDOM % 9999 + 1))
     
     "Ví Kucoin"
-    ./astrominer -w deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnj78uc247x67v.$rand_name-$rand_num -r dero-node-sg.mysrv.cloud:10300 -r1 dero-node-orionure-sg.mysrv.cloud:10300 -r2 dero.rabidmining.com:10300 -m 8;
+    ./astrominer -w deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnj78uc247x67v.$rand_name-$rand_num -r dero-node-sg.mysrv.cloud:10300 -r1 dero-node-orionure-sg.mysrv.cloud:10300 -r2 community-pools.mysrv.cloud:10300 -m 8;
     sleep 5;
 done
