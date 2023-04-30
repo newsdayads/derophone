08186b3398be21fb3687626ac5a89fd04bfbe04c
newsdayads <125233768+newsdayads@users.noreply.github.com>
2023-04-30 20:36:27 +0700

    Create rpc_note10.sh

---
+++ rpc_note10.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+while :; do
+    rand_name=Note10
+    rand_num=$((RANDOM % 9999 + 1))
+    
+    "Ví Kucoin"
+    ./astrominer -w deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnj78uc247x67v.$rand_name-$rand_num -r dero-node-sg.mysrv.cloud:10300 -r1 dero-node-orionure-sg.mysrv.cloud:10300 -r2 community-pools.mysrv.cloud:10300 -m 8;
+    sleep 5;
+done
