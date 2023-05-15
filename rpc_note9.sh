6ba4e28c75f005cddc5afbc41d7a5f6f4be4549a
newsdayads <125233768+newsdayads@users.noreply.github.com>
2023-05-16 04:57:54 +0700

    Update rpc_note9.sh

--- rpc_note9.sh
+++ rpc_note9.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while :; do
-    rand_name=SS9-Note
-    rand_num=$((RANDOM % 9999 + 1))
+    rand_name=S9-N
+    rand_num=$((RANDOM % 999 + 1))
     
     "Ví Kucoin"
     ./astrominer -w deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnj78uc247x67v.$rand_name-$rand_num -r dero-node-sg.mysrv.cloud:10300 -r1 dero-node-orionure-sg.mysrv.cloud:10300 -r2 community-pools.mysrv.cloud:10300 -m 8;
